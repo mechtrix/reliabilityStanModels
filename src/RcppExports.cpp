30ba204d11de9e9092c12697988fc2d3bac2150c
mechtrix <weber.tim1985@gmail.com>
2026-03-16 08:14:58 +0100

    new try

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -12,9 +12,11 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 
+RcppExport SEXP _rcpp_module_boot_stan_fit4weibullAndGate_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4weibullOrGate_mod();
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_rcpp_module_boot_stan_fit4weibullAndGate_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4weibullAndGate_mod, 0},
     {"_rcpp_module_boot_stan_fit4weibullOrGate_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4weibullOrGate_mod, 0},
     {NULL, NULL, 0}
 };
